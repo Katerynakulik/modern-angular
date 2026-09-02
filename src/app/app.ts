ddb69cd48327825f3cd505bf5791a8d7d25a3e89
Kateryna Kulik <katerynakulik4@gmail.com>
2026-09-02 15:55:48 +0200

    Create structure/grid and card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { ProductsGrid } from './products/products-grid/products-grid';
 
 @Component({
-  imports: [RouterOutlet,  Header],
+  imports: [RouterOutlet,  Header, ProductsGrid],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
