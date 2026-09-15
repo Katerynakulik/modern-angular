a0becfc4f36beaf9563a90fab16986172aceee46
Kateryna Kulik <katerynakulik4@gmail.com>
2026-09-15 11:46:03 +0200

    Fill the grid with a proper info, from backend

--- src/app/products/product-card/product-card.ts
+++ src/app/products/product-card/product-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Product } from './../product';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
@@ -8,4 +9,7 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './product-card.scss',
   templateUrl: './product-card.html',
 })
-export class ProductCard {}
+export class ProductCard {
+  readonly product = input.required<Product>();
+  readonly addButtonLabel = input('Add to Cart');
+}
