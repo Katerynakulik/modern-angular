4a606f1bc444410d7cde1ef5b20d24d677b8cf98
Kateryna Kulik <katerynakulik4@gmail.com>
2026-09-17 13:16:01 +0200

    Add badge to cart

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CartService } from './../cart/cart-service';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatBadge],
   selector: 'app-header',
   styleUrl: './header.scss',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  protected readonly cartService =inject(CartService);
+}
